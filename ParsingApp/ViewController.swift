cd4545bdfd5437779a6a15efd008ff49175ce8af
Elena <elmikhailova86@gmail.com>
2022-12-14 19:50:55 +0300

    Initial Commit

---
+++ ParsingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParsingApp
+//
+//  Created by Elena Mikhailova on 14.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
